a5d7796dd35d33dca2b6a61139d129a900c14042
airtonsena <airtonsena41@gmail.com>
2021-08-31 16:34:21 -0300

    Autenticaçâo com NextAuth😍

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -2,25 +2,26 @@ import styles from "./styles.module.scss";
 
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from 'next-auth/client';
 
 export function SignInButton() {
-  const isUserLoggedIn = false;
+  const [session] = useSession();
 
-  return isUserLoggedIn ? (
+  return session? (
    <button 
    type="button" 
    className={styles.signInButton}
-  
+   onClick={() => signOut()}
    >
    <FaGithub color="#04d361" />
-   Airton Sena
+   {session.user.name}
    <FiX color="#737380" className={styles.closeIcon} />
 </button>
 ) : (
 <button 
    type="button" 
    className={styles.signInButton}
-   
+   onClick={() => signIn('github')}
    >
    <FaGithub color="#eba417" />
    Sign in with Github
